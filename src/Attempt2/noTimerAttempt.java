758767264f3476496d8182e421c6f0f48393a2fe
Michael A <michael@michaels-air.home>
2023-03-07 15:07:50 +0000

    comments added to 2nd attempt

--- src/Attempt2/noTimerAttempt.java
+++ src/Attempt2/noTimerAttempt.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 
 public class noTimerAttempt {
 
-
+//I want to create a list of numbers with the defined parameters
 
     public static void main(String[] args) {
 
@@ -22,22 +22,26 @@ public class noTimerAttempt {
         list.add(11);
         list.add(98);
 
-
+    // list has been created then print to console to see the 10 entries
         System.out.println(list);
+
+        //Create a for loop to loop through each index, and for each index found check to see if they equal to another index found in the list
 
         for (int i = 0; i < list.size(); i++) {
             for (int j=1; j < list.size(); j++){
+
                 if (list.get(i) == list.get(j))
+
+                    //if an instance where i = j is found remove constant j from the list
                     list.remove(j);
             }
 
         }
-        //sortInDescendingOrder();
-
+        //We now then print the updated list without the duplications
         System.out.println(list);
-
+        //now we need to sort out list into descending order, so call onto the collection method, and instansiate the list to appear in reverse order
         Collections.sort(list, Collections.reverseOrder());
-
+        // print out reverse order onto console
         System.out.println("This is in reversed Order" + "" + list);
 
     }
